cd250ce3f9cd004e40a7f11d797f69d95d3dabae
Jakub Hrášek <hrasekj@gmail.com>
2016-11-25 15:33:08 +0100

    Add controllers

---
+++ controllers/Controller.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @Author: Jakub Hrášek
+ * @Date:   2016-11-24 03:33:47
+ * @Last Modified by:   Jakub Hrášek
+ * @Last Modified time: 2016-11-25 14:24:28
+ */
+
+namespace api\controllers;
+
+
+use Yii;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBasicAuth;
+use yii\filters\ContentNegotiator;
+use yii\filters\RateLimiter;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+
+class Controller extends \yii\rest\Controller
+{
+	/**
+	 * @var string|array the configuration for creating the serializer that formats the response data.
+	 */
+	public $serializer = [
+		'class' => 'yii\rest\Serializer',
+		'collectionEnvelope' => 'items',
+	];
+
+
+	/**
+	 * @inheritdoc
+	 */
+	public function behaviors()
+	{
+		return [
+			'contentNegotiator' => [
+				'class' => ContentNegotiator::className(),
+				'formats' => [
+					'application/json' => Response::FORMAT_JSON,
+				],
+			],
+			'verbFilter' => [
+				'class' => VerbFilter::className(),
+				'actions' => $this->verbs(),
+			],
+			'authenticator' => [
+				'class' => CompositeAuth::className(),
+				// 'authMethods' => [HttpBasicAuth::className()],
+			],
+			'rateLimiter' => [
+				'class' => RateLimiter::className(),
+			],
+		];
+	}
+
+}
---
+++ controllers/DefaultController.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @Author: Jakub Hrášek
+ * @Date:   2016-11-25 12:19:51
+ * @Last Modified by:   Jakub Hrášek
+ * @Last Modified time: 2016-11-25 15:29:07
+ */
+
+namespace api\controllers;
+
+
+use Yii;
+
+class DefaultController extends ActiveController
+{
+	/**
+	 * @inheritdoc
+	 */
+	public function init()
+	{
+		if ($this->modelClass === null) {
+			$this->modelClass = $this->module->modelNamespace . '\\' . self::dashesToCamelCase($this->id);
+		}
+
+		parent::init();
+	}
+
+
+	/**
+	 * Convert dashed string from url to camel case.
+	 *
+	 * @param string dashed string.
+	 * @param boolean capitalize first character
+	 * @return string camel cased string
+	 */
+	public static function dashesToCamelCase($string, $capitalizeFirstCharacter = true)
+	{
+		$str = str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
+
+		if (!$capitalizeFirstCharacter) {
+			$str[0] = strtolower($str[0]);
+		}
+
+		return $str;
+	}
+
+}
